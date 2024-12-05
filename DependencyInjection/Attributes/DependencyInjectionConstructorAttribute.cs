cd9cc14f94d19af8c565800ecca73f30fcb9b783
Piotr Zagóra <piotr@zagora.dev>
2024-12-05 11:11:26 +0100

    Added XML documentation for attributes

--- DependencyInjection/Attributes/DependencyInjectionConstructorAttribute.cs
+++ DependencyInjection/Attributes/DependencyInjectionConstructorAttribute.cs
@@ -3,6 +3,10 @@ using JetBrains.Annotations;
 
 namespace DependencyInjection.Attributes
 {
+    /// <summary>
+    /// Marks constructor to specifically use for injection.
+    /// When missing, constructor with most properties will be used.
+    /// </summary>
     [MeansImplicitUse(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
     [AttributeUsage(AttributeTargets.Constructor)]
     public class DependencyInjectionConstructorAttribute : Attribute
--- DependencyInjection/Attributes/InjectAttribute.cs
+++ DependencyInjection/Attributes/InjectAttribute.cs
@@ -3,6 +3,9 @@ using JetBrains.Annotations;
 
 namespace DependencyInjection.Attributes
 {
+    /// <summary>
+    /// Marks Field, Property or Method for injection.
+    /// </summary>
     [MeansImplicitUse(ImplicitUseKindFlags.Assign)]
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
     public class InjectAttribute : Attribute
